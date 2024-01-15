4c7516ca458e2578c803181c237924b92db1b0fd
medasravanthi <150466318+medasravanthi@users.noreply.github.com>
2024-01-15 12:43:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,35 @@
-############### Main Variables ##################
 variable "main_allocated_storage"{
 type = number
-default = "20"
+default ="20"
 }
 variable "main_db_engine"{
 type = string
-default = "MYSQL"
+default ="MySQL"
 }
 variable "main_db_instance_class"{
 type = string
-default = "db.t2.micro"
+default ="db.t2.micro"
 }
 variable "main_db_username" {
 type = string  
-default = "admin"
+default ="admin"
 }
 variable "parameter_group_name" {
     type = string
-default = "default.mysql8.0"
+    default ="default.mysql8.0" 
 }
 variable "password" {
-type = string  
-default = "baby.sravs"
+type = string 
+default = "baby.sravs" 
 }
 variable "main_db_version" {
     type = string
-default = "8.0.35"
-  
+  default ="8.0.35"
 }
 #################replica##############
 variable "replica_instance_class" {
     type = string
-default = "db.t2.micro"
+    default = "db.t2.micro"
   
 }
 
@@ -42,7 +40,7 @@ variable "replica_count" {
 
 variable "rds_replica_azs" {
   type = list(string)
-  default = ["eu-west-2c"]
+  default = ["us-west-2c"]
 }
 variable "sg_vpc_id" {
     type = string
@@ -54,18 +52,19 @@ variable "sg_ingress_from_port" {
 }
 variable "sg_ingress_to_port" {
     type = number
-default = "3306"
-  
+  default ="3306"
 }
-variable "cidr"{
+ variable "cidr"{
 type = list(string)
 default = ["0.0.0.0/0"]
 }
+
 variable "sg_protocol" {
     type = string
-default = "tcp"
+    default = "tcp"
 }
 variable "sg_name" {
   type = string
-default = "name"
+  default ="name"
 }
+
